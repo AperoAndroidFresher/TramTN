abc29e73ab73a0d9654e663d26e3807434100c13
tramtn910 <tramtn@aperan.vn>
2025-01-17 16:53:59 +0700

    add field1

--- app/src/main/java/com/example/musicapp/dangki/User.kt
+++ app/src/main/java/com/example/musicapp/dangki/User.kt
@@ -1,12 +1,15 @@
 package com.example.musicapp.dangki
 
 fun main(){
+    val validateFields = ValidateFields()
     val user = User(
         username = "user123",
         email = "test@apero.vn",
         password = "Password123",
         phoneNumber = "01234567890"
     )
+    val isValidUsername = validateFields.field1(user.username)
+    println(isValidUsername)
 
 }
 
@@ -16,3 +19,9 @@ class User(
     val password: String,
     val phoneNumber: String
 )
+class ValidateFields{
+    fun field1(name: String): Boolean {
+        val usernameRegex = Regex("^[a-zA-Z0-9]+\$")
+        return name.matches(usernameRegex)
+    }
+}
