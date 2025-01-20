9decb22e0076569b60d373672bcd7cb9c8c256e0
tramtn910 <tramtn@apero.vn>
2025-01-20 23:16:17 +0700

    update UI for login/signin

--- app/src/main/java/com/example/musicapp/LoginActivity.kt
+++ app/src/main/java/com/example/musicapp/LoginActivity.kt
@@ -2,7 +2,8 @@ package com.example.musicapp
 
 import android.content.Intent
 import android.os.Bundle
-
+import android.text.InputType
+import android.view.MotionEvent
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
@@ -10,17 +11,17 @@ import androidx.appcompat.app.AppCompatActivity
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var usernameField: EditText
-
+    private lateinit var passwordField: EditText
     private lateinit var usernameError: TextView
     private lateinit var passwordError: TextView
     private lateinit var loginButton: Button
     private lateinit var signupText: TextView
-    private lateinit var passwordField: EditText
-
 
     private val userList = hashMapOf(
         "tram" to "Tram123"
     )
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +33,6 @@ class LoginActivity : AppCompatActivity() {
         passwordError = findViewById(R.id.passwordError)
         loginButton = findViewById(R.id.loginButton)
         signupText = findViewById(R.id.signupText)
-
 
         loginButton.setOnClickListener {
             val username = usernameField.text.toString().trim()
@@ -76,7 +76,7 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
-
     }
-}
 
+
+}
